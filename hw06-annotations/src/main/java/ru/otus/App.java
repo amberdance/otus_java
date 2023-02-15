a149cf583d4f7c4005731f651cd49f28d2cda688
Alexey Fishler <amberdance@yandex.ru>
2023-02-15 23:53:13 +0300

    [refactoring] remove another hw nodules

--- hw06-annotations/src/main/java/ru/otus/App.java
+++
@@ -1,9 +0,0 @@
-package ru.otus;
-
-import ru.otus.test.TestController;
-
-public class App {
-    public static void main(String[] args) {
-        new TestController(PangramCheckerTest.class).init();
-    }
-}
