b87e65040080327359c2ef28a3bd22744b7ed193
Alexey Fishler <amberdance@yandex.ru>
2023-02-17 11:03:57 +0300

    [fix] cleaned dead code

--- hw12-solid/src/main/java/ru/otus/solid/exception/CapacityExhaustException.java
+++ hw12-solid/src/main/java/ru/otus/solid/exception/CapacityExhaustException.java
@@ -2,7 +2,7 @@ package ru.otus.solid.exception;
 
 public class CapacityExhaustException extends RuntimeException {
 
-    public static String defaultMessage = "Cannot withdrew %d. Total available: %d";
+    public static final String defaultMessage = "Cannot withdrew %d. Total available: %d";
 
     public CapacityExhaustException(String message) {
         super(message);
--- hw12-solid/src/main/java/ru/otus/solid/interfaces/BanknoteSlot.java
+++ hw12-solid/src/main/java/ru/otus/solid/interfaces/BanknoteSlot.java
@@ -3,22 +3,9 @@ package ru.otus.solid.interfaces;
 import ru.otus.solid.atm.Nominal;
 import ru.otus.solid.exception.NotEnoughBanknotesException;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public interface BanknoteSlot {
 
-    public static final int DEFAULT_NOMINAL_COUNT = 100;
-
-    default Map<Nominal, Integer> getEmptySlots() {
-        var slots = new HashMap<Nominal, Integer>();
-
-        for (Nominal nominal : Nominal.values()) {
-            slots.put(nominal, 0);
-        }
-
-        return slots;
-    }
+    int DEFAULT_NOMINAL_COUNT = 100;
 
     void take(Nominal nominal, int count) throws NotEnoughBanknotesException;
 
