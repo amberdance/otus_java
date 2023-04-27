2e55ab6647e0ade280c702b6a9127bada34c7b0c
Alexey Fishler <amberdance@yandex.ru>
2023-04-27 22:42:28 +0300

    hw18-jdbc

--- hw16-io/src/main/java/ru/otus/dataprocessor/Serializer.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.dataprocessor;
-
-import java.io.IOException;
-import java.util.Map;
-
-public interface Serializer {
-
-    void serialize(Map<String, Double> data) throws IOException;
-}
