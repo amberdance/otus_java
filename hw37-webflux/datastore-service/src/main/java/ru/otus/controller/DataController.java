e0afa3156a429d10f5c37a4e2e5186279c58bc33
Alexey Fishler <amberdance@yandex.ru>
2023-08-04 20:39:31 +0300

    Поправил сообщения логов (для лучшего понимания для себя)

--- hw37-webflux/datastore-service/src/main/java/ru/otus/controller/DataController.java
+++ hw37-webflux/datastore-service/src/main/java/ru/otus/controller/DataController.java
@@ -4,7 +4,11 @@ package ru.otus.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
@@ -25,14 +29,14 @@ public class DataController {
                                       @RequestBody MessageDto messageDto) {
         var message = messageDto.message();
         var msgId = Mono.just(new Message(roomId, message))
-                .doOnNext(msg -> log.info("messageFromChat:{}", msg))
+                .doOnNext(msg -> log.info("Got message: {}", msg))
                 .flatMap(dataStoreService::saveMessage)
                 .publishOn(workerPool)
-                .doOnNext(msgSaved -> log.info("msgSaved id:{}", msgSaved.getId()))
+                .doOnNext(msgSaved -> log.info("Message saved with id: {}", msgSaved.getId()))
                 .map(Message::getId)
                 .subscribeOn(workerPool);
 
-        log.info("messageFromChat, roomId:{}, msg:{} done", roomId, message);
+        log.info("Got message from chat, Room Id: {}, Message: {}", roomId, message);
 
         return msgId;
     }
@@ -40,20 +44,20 @@ public class DataController {
     @GetMapping(value = "/msg/{roomId}", produces = MediaType.APPLICATION_NDJSON_VALUE)
     public Flux<MessageDto> getMessagesByRoomId(@PathVariable("roomId") String roomId) {
         return Mono.just(roomId)
-                .doOnNext(room -> log.info("getMessagesByRoomId, room:{}", room))
+                .doOnNext(room -> log.info("Got message by rom id: {}", room))
                 .flatMapMany(dataStoreService::loadMessages)
                 .map(message -> new MessageDto(message.getMsgText()))
-                .doOnNext(msgDto -> log.info("msgDto:{}", msgDto))
+                .doOnNext(msgDto -> log.info("Converted message to dto: {}", msgDto))
                 .subscribeOn(workerPool);
     }
 
     @GetMapping(value = "/msg/all", produces = MediaType.APPLICATION_NDJSON_VALUE)
     public Flux<MessageDto> getAllMassages() {
         return Mono.just("all")
-                .doOnNext(room -> log.info("getMessagesByRoomId, room:{}", room))
+                .doOnNext(room -> log.info("Getting all messages, room: {}", room))
                 .flatMapMany(dataStoreService::loadAllMessages)
                 .map(message -> new MessageDto(message.getMsgText()))
-                .doOnNext(msgDto -> log.info("msgDto:{}", msgDto))
+                .doOnNext(msgDto -> log.info("Converted message to dto: {}", msgDto))
                 .subscribeOn(workerPool);
     }
 }
